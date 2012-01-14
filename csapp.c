61055214820000a3a4d0a4bbd0e14202240ac9c6
mofaph <mofaph@gmail.com>
2012-01-14 13:14:30 +0800

    将制表符替换为空格

--- csapp.c
+++ csapp.c
@@ -42,7 +42,7 @@ pid_t Fork(void)
     pid_t pid;
 
     if ((pid = fork()) < 0)
-	unix_error("Fork error");
+        unix_error("Fork error");
     return pid;
 }
 /* $end forkwrapper */
@@ -50,7 +50,7 @@ pid_t Fork(void)
 void Execve(const char *filename, char *const argv[], char *const envp[])
 {
     if (execve(filename, argv, envp) < 0)
-	unix_error("Execve error");
+        unix_error("Execve error");
 }
 
 /* $begin wait */
@@ -59,7 +59,7 @@ pid_t Wait(int *status)
     pid_t pid;
 
     if ((pid  = wait(status)) < 0)
-	unix_error("Wait error");
+        unix_error("Wait error");
     return pid;
 }
 /* $end wait */
@@ -69,7 +69,7 @@ pid_t Waitpid(pid_t pid, int *iptr, int options)
     pid_t retpid;
 
     if ((retpid  = waitpid(pid, iptr, options)) < 0)
-	unix_error("Waitpid error");
+        unix_error("Waitpid error");
     return(retpid);
 }
 
@@ -79,7 +79,7 @@ void Kill(pid_t pid, int signum)
     int rc;
 
     if ((rc = kill(pid, signum)) < 0)
-	unix_error("Kill error");
+        unix_error("Kill error");
 }
 /* $end kill */
 
@@ -94,7 +94,7 @@ unsigned int Sleep(unsigned int secs)
     unsigned int rc;
 
     if ((rc = sleep(secs)) < 0)
-	unix_error("Sleep error");
+        unix_error("Sleep error");
     return rc;
 }
 
@@ -106,7 +106,7 @@ void Setpgid(pid_t pid, pid_t pgid) {
     int rc;
 
     if ((rc = setpgid(pid, pgid)) < 0)
-	unix_error("Setpgid error");
+        unix_error("Setpgid error");
     return;
 }
 
@@ -128,7 +128,7 @@ handler_t *Signal(int signum, handler_t *handler)
     action.sa_flags = SA_RESTART; /* restart syscalls if possible */
 
     if (sigaction(signum, &action, &old_action) < 0)
-	unix_error("Signal error");
+        unix_error("Signal error");
     return (old_action.sa_handler);
 }
 /* $end sigaction */
@@ -136,35 +136,35 @@ handler_t *Signal(int signum, handler_t *handler)
 void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
 {
     if (sigprocmask(how, set, oldset) < 0)
-	unix_error("Sigprocmask error");
+        unix_error("Sigprocmask error");
     return;
 }
 
 void Sigemptyset(sigset_t *set)
 {
     if (sigemptyset(set) < 0)
-	unix_error("Sigemptyset error");
+        unix_error("Sigemptyset error");
     return;
 }
 
 void Sigfillset(sigset_t *set)
 {
     if (sigfillset(set) < 0)
-	unix_error("Sigfillset error");
+        unix_error("Sigfillset error");
     return;
 }
 
 void Sigaddset(sigset_t *set, int signum)
 {
     if (sigaddset(set, signum) < 0)
-	unix_error("Sigaddset error");
+        unix_error("Sigaddset error");
     return;
 }
 
 void Sigdelset(sigset_t *set, int signum)
 {
     if (sigdelset(set, signum) < 0)
-	unix_error("Sigdelset error");
+        unix_error("Sigdelset error");
     return;
 }
 
@@ -172,7 +172,7 @@ int Sigismember(const sigset_t *set, int signum)
 {
     int rc;
     if ((rc = sigismember(set, signum)) < 0)
-	unix_error("Sigismember error");
+        unix_error("Sigismember error");
     return rc;
 }
 
@@ -186,7 +186,7 @@ int Open(const char *pathname, int flags, mode_t mode)
     int rc;
 
     if ((rc = open(pathname, flags, mode))  < 0)
-	unix_error("Open error");
+        unix_error("Open error");
     return rc;
 }
 
@@ -195,7 +195,7 @@ ssize_t Read(int fd, void *buf, size_t count)
     ssize_t rc;
 
     if ((rc = read(fd, buf, count)) < 0)
-	unix_error("Read error");
+        unix_error("Read error");
     return rc;
 }
 
@@ -204,7 +204,7 @@ ssize_t Write(int fd, const void *buf, size_t count)
     ssize_t rc;
 
     if ((rc = write(fd, buf, count)) < 0)
-	unix_error("Write error");
+        unix_error("Write error");
     return rc;
 }
 
@@ -213,7 +213,7 @@ off_t Lseek(int fildes, off_t offset, int whence)
     off_t rc;
 
     if ((rc = lseek(fildes, offset, whence)) < 0)
-	unix_error("Lseek error");
+        unix_error("Lseek error");
     return rc;
 }
 
@@ -222,16 +222,16 @@ void Close(int fd)
     int rc;
 
     if ((rc = close(fd)) < 0)
-	unix_error("Close error");
+        unix_error("Close error");
 }
 
 int Select(int  n, fd_set *readfds, fd_set *writefds,
-	   fd_set *exceptfds, struct timeval *timeout)
+           fd_set *exceptfds, struct timeval *timeout)
 {
     int rc;
 
     if ((rc = select(n, readfds, writefds, exceptfds, timeout)) < 0)
-	unix_error("Select error");
+        unix_error("Select error");
     return rc;
 }
 
@@ -240,20 +240,20 @@ int Dup2(int fd1, int fd2)
     int rc;
 
     if ((rc = dup2(fd1, fd2)) < 0)
-	unix_error("Dup2 error");
+        unix_error("Dup2 error");
     return rc;
 }
 
 void Stat(const char *filename, struct stat *buf)
 {
     if (stat(filename, buf) < 0)
-	unix_error("Stat error");
+        unix_error("Stat error");
 }
 
 void Fstat(int fd, struct stat *buf)
 {
     if (fstat(fd, buf) < 0)
-	unix_error("Fstat error");
+        unix_error("Fstat error");
 }
 
 /***************************************
@@ -264,14 +264,14 @@ void *Mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
     void *ptr;
 
     if ((ptr = mmap(addr, len, prot, flags, fd, offset)) == ((void *) -1))
-	unix_error("mmap error");
+        unix_error("mmap error");
     return(ptr);
 }
 
 void Munmap(void *start, size_t length)
 {
     if (munmap(start, length) < 0)
-	unix_error("munmap error");
+        unix_error("munmap error");
 }
 
 /***************************************************
@@ -283,7 +283,7 @@ void *Malloc(size_t size)
     void *p;
 
     if ((p  = malloc(size)) == NULL)
-	unix_error("Malloc error");
+        unix_error("Malloc error");
     return p;
 }
 
@@ -292,7 +292,7 @@ void *Realloc(void *ptr, size_t size)
     void *p;
 
     if ((p  = realloc(ptr, size)) == NULL)
-	unix_error("Realloc error");
+        unix_error("Realloc error");
     return p;
 }
 
@@ -301,7 +301,7 @@ void *Calloc(size_t nmemb, size_t size)
     void *p;
 
     if ((p = calloc(nmemb, size)) == NULL)
-	unix_error("Calloc error");
+        unix_error("Calloc error");
     return p;
 }
 
@@ -316,7 +316,7 @@ void Free(void *ptr)
 void Fclose(FILE *fp)
 {
     if (fclose(fp) != 0)
-	unix_error("Fclose error");
+        unix_error("Fclose error");
 }
 
 FILE *Fdopen(int fd, const char *type)
@@ -324,7 +324,7 @@ FILE *Fdopen(int fd, const char *type)
     FILE *fp;
 
     if ((fp = fdopen(fd, type)) == NULL)
-	unix_error("Fdopen error");
+        unix_error("Fdopen error");
 
     return fp;
 }
@@ -334,7 +334,7 @@ char *Fgets(char *ptr, int n, FILE *stream)
     char *rptr;
 
     if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
-	app_error("Fgets error");
+        app_error("Fgets error");
 
     return rptr;
 }
@@ -344,7 +344,7 @@ FILE *Fopen(const char *filename, const char *mode)
     FILE *fp;
 
     if ((fp = fopen(filename, mode)) == NULL)
-	unix_error("Fopen error");
+        unix_error("Fopen error");
 
     return fp;
 }
@@ -352,7 +352,7 @@ FILE *Fopen(const char *filename, const char *mode)
 void Fputs(const char *ptr, FILE *stream)
 {
     if (fputs(ptr, stream) == EOF)
-	unix_error("Fputs error");
+        unix_error("Fputs error");
 }
 
 size_t Fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
@@ -360,14 +360,14 @@ size_t Fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
     size_t n;
 
     if (((n = fread(ptr, size, nmemb, stream)) < nmemb) && ferror(stream))
-	unix_error("Fread error");
+        unix_error("Fread error");
     return n;
 }
 
 void Fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 {
     if (fwrite(ptr, size, nmemb, stream) < nmemb)
-	unix_error("Fwrite error");
+        unix_error("Fwrite error");
 }
 
 
@@ -380,7 +380,7 @@ int Socket(int domain, int type, int protocol)
     int rc;
 
     if ((rc = socket(domain, type, protocol)) < 0)
-	unix_error("Socket error");
+        unix_error("Socket error");
     return rc;
 }
 
@@ -389,7 +389,7 @@ void Setsockopt(int s, int level, int optname, const void *optval, int optlen)
     int rc;
 
     if ((rc = setsockopt(s, level, optname, optval, optlen)) < 0)
-	unix_error("Setsockopt error");
+        unix_error("Setsockopt error");
 }
 
 void Bind(int sockfd, struct sockaddr *my_addr, int addrlen)
@@ -397,7 +397,7 @@ void Bind(int sockfd, struct sockaddr *my_addr, int addrlen)
     int rc;
 
     if ((rc = bind(sockfd, my_addr, addrlen)) < 0)
-	unix_error("Bind error");
+        unix_error("Bind error");
 }
 
 void Listen(int s, int backlog)
@@ -405,7 +405,7 @@ void Listen(int s, int backlog)
     int rc;
 
     if ((rc = listen(s,  backlog)) < 0)
-	unix_error("Listen error");
+        unix_error("Listen error");
 }
 
 int Accept(int s, struct sockaddr *addr, socklen_t *addrlen)
@@ -413,7 +413,7 @@ int Accept(int s, struct sockaddr *addr, socklen_t *addrlen)
     int rc;
 
     if ((rc = accept(s, addr, addrlen)) < 0)
-	unix_error("Accept error");
+        unix_error("Accept error");
     return rc;
 }
 
@@ -422,7 +422,7 @@ void Connect(int sockfd, struct sockaddr *serv_addr, int addrlen)
     int rc;
 
     if ((rc = connect(sockfd, serv_addr, addrlen)) < 0)
-	unix_error("Connect error");
+        unix_error("Connect error");
 }
 
 /************************
@@ -435,7 +435,7 @@ struct hostent *Gethostbyname(const char *name)
     struct hostent *p;
 
     if ((p = gethostbyname(name)) == NULL)
-	dns_error("Gethostbyname error");
+        dns_error("Gethostbyname error");
     return p;
 }
 /* $end gethostbyname */
@@ -445,7 +445,7 @@ struct hostent *Gethostbyaddr(const char *addr, int len, int type)
     struct hostent *p;
 
     if ((p = gethostbyaddr(addr, len, type)) == NULL)
-	dns_error("Gethostbyaddr error");
+        dns_error("Gethostbyaddr error");
     return p;
 }
 
@@ -454,26 +454,26 @@ struct hostent *Gethostbyaddr(const char *addr, int len, int type)
  ************************************************/
 
 void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp,
-		    void * (*routine)(void *), void *argp)
+                    void * (*routine)(void *), void *argp)
 {
     int rc;
 
     if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
-	posix_error(rc, "Pthread_create error");
+        posix_error(rc, "Pthread_create error");
 }
 
 void Pthread_cancel(pthread_t tid) {
     int rc;
 
     if ((rc = pthread_cancel(tid)) != 0)
-	posix_error(rc, "Pthread_cancel error");
+        posix_error(rc, "Pthread_cancel error");
 }
 
 void Pthread_join(pthread_t tid, void **thread_return) {
     int rc;
 
     if ((rc = pthread_join(tid, thread_return)) != 0)
-	posix_error(rc, "Pthread_join error");
+        posix_error(rc, "Pthread_join error");
 }
 
 /* $begin detach */
@@ -481,7 +481,7 @@ void Pthread_detach(pthread_t tid) {
     int rc;
 
     if ((rc = pthread_detach(tid)) != 0)
-	posix_error(rc, "Pthread_detach error");
+        posix_error(rc, "Pthread_detach error");
 }
 /* $end detach */
 
@@ -504,19 +504,19 @@ void Pthread_once(pthread_once_t *once_control, void (*init_function)()) {
 void Sem_init(sem_t *sem, int pshared, unsigned int value)
 {
     if (sem_init(sem, pshared, value) < 0)
-	unix_error("Sem_init error");
+        unix_error("Sem_init error");
 }
 
 void P(sem_t *sem)
 {
     if (sem_wait(sem) < 0)
-	unix_error("P error");
+        unix_error("P error");
 }
 
 void V(sem_t *sem)
 {
     if (sem_post(sem) < 0)
-	unix_error("V error");
+        unix_error("V error");
 }
 
 /*********************************************************************
@@ -533,16 +533,16 @@ ssize_t rio_readn(int fd, void *usrbuf, size_t n)
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nread = read(fd, bufp, nleft)) < 0) {
-	    if (errno == EINTR) /* interrupted by sig handler return */
-		nread = 0;      /* and call read() again */
-	    else
-		return -1;      /* errno set by read() */
-	}
-	else if (nread == 0)
-	    break;              /* EOF */
-	nleft -= nread;
-	bufp += nread;
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) /* interrupted by sig handler return */
+                nread = 0;      /* and call read() again */
+            else
+                return -1;      /* errno set by read() */
+        }
+        else if (nread == 0)
+            break;              /* EOF */
+        nleft -= nread;
+        bufp += nread;
     }
     return (n - nleft);         /* return >= 0 */
 }
@@ -559,14 +559,14 @@ ssize_t rio_writen(int fd, void *usrbuf, size_t n)
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nwritten = write(fd, bufp, nleft)) <= 0) {
-	    if (errno == EINTR)  /* interrupted by sig handler return */
-		nwritten = 0;    /* and call write() again */
-	    else
-		return -1;       /* errno set by write() */
-	}
-	nleft -= nwritten;
-	bufp += nwritten;
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)  /* interrupted by sig handler return */
+                nwritten = 0;    /* and call write() again */
+            else
+                return -1;       /* errno set by write() */
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
     }
     return n;
 }
@@ -587,22 +587,22 @@ static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
     int cnt;
 
     while (rp->rio_cnt <= 0) {  /* refill if buf is empty */
-	rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
-			   sizeof(rp->rio_buf));
-	if (rp->rio_cnt < 0) {
-	    if (errno != EINTR) /* interrupted by sig handler return */
-		return -1;
-	}
-	else if (rp->rio_cnt == 0)  /* EOF */
-	    return 0;
-	else
-	    rp->rio_bufptr = rp->rio_buf; /* reset buffer ptr */
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
+                           sizeof(rp->rio_buf));
+        if (rp->rio_cnt < 0) {
+            if (errno != EINTR) /* interrupted by sig handler return */
+                return -1;
+        }
+        else if (rp->rio_cnt == 0)  /* EOF */
+            return 0;
+        else
+            rp->rio_bufptr = rp->rio_buf; /* reset buffer ptr */
     }
 
     /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
     cnt = n;
     if (rp->rio_cnt < n)
-	cnt = rp->rio_cnt;
+        cnt = rp->rio_cnt;
     memcpy(usrbuf, rp->rio_bufptr, cnt);
     rp->rio_bufptr += cnt;
     rp->rio_cnt -= cnt;
@@ -633,16 +633,16 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nread = rio_read(rp, bufp, nleft)) < 0) {
-	    if (errno == EINTR) /* interrupted by sig handler return */
-		nread = 0;      /* call read() again */
-	    else
-		return -1;      /* errno set by read() */
-	}
-	else if (nread == 0)
-	    break;              /* EOF */
-	nleft -= nread;
-	bufp += nread;
+        if ((nread = rio_read(rp, bufp, nleft)) < 0) {
+            if (errno == EINTR) /* interrupted by sig handler return */
+                nread = 0;      /* call read() again */
+            else
+                return -1;      /* errno set by read() */
+        }
+        else if (nread == 0)
+            break;              /* EOF */
+        nleft -= nread;
+        bufp += nread;
     }
     return (n - nleft);         /* return >= 0 */
 }
@@ -658,17 +658,17 @@ ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
     char c, *bufp = usrbuf;
 
     for (n = 1; n < maxlen; n++) {
-	if ((rc = rio_read(rp, &c, 1)) == 1) {
-	    *bufp++ = c;
-	    if (c == '\n')
-		break;
-	} else if (rc == 0) {
-	    if (n == 1)
-		return 0; /* EOF, no data read */
-	    else
-		break;    /* EOF, some data was read */
-	} else
-	    return -1;	  /* error */
+        if ((rc = rio_read(rp, &c, 1)) == 1) {
+            *bufp++ = c;
+            if (c == '\n')
+                break;
+        } else if (rc == 0) {
+            if (n == 1)
+                return 0; /* EOF, no data read */
+            else
+                break;    /* EOF, some data was read */
+        } else
+            return -1;    /* error */
     }
     *bufp = 0;
     return n;
@@ -683,14 +683,14 @@ ssize_t Rio_readn(int fd, void *ptr, size_t nbytes)
     ssize_t n;
 
     if ((n = rio_readn(fd, ptr, nbytes)) < 0)
-	unix_error("Rio_readn error");
+        unix_error("Rio_readn error");
     return n;
 }
 
 void Rio_writen(int fd, void *usrbuf, size_t n)
 {
     if (rio_writen(fd, usrbuf, n) != n)
-	unix_error("Rio_writen error");
+        unix_error("Rio_writen error");
 }
 
 void Rio_readinitb(rio_t *rp, int fd)
@@ -703,7 +703,7 @@ ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n)
     ssize_t rc;
 
     if ((rc = rio_readnb(rp, usrbuf, n)) < 0)
-	unix_error("Rio_readnb error");
+        unix_error("Rio_readnb error");
     return rc;
 }
 
@@ -712,7 +712,7 @@ ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
     ssize_t rc;
 
     if ((rc = rio_readlineb(rp, usrbuf, maxlen)) < 0)
-	unix_error("Rio_readlineb error");
+        unix_error("Rio_readlineb error");
     return rc;
 }
 
@@ -733,20 +733,20 @@ int open_clientfd(char *hostname, int port)
     struct sockaddr_in serveraddr;
 
     if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	return -1; /* check errno for cause of error */
+        return -1; /* check errno for cause of error */
 
     /* Fill in the server's IP address and port */
     if ((hp = gethostbyname(hostname)) == NULL)
-	return -2; /* check h_errno for cause of error */
+        return -2; /* check h_errno for cause of error */
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     bcopy((char *)hp->h_addr_list[0],
-	  (char *)&serveraddr.sin_addr.s_addr, hp->h_length);
+          (char *)&serveraddr.sin_addr.s_addr, hp->h_length);
     serveraddr.sin_port = htons(port);
 
     /* Establish a connection with the server */
     if (connect(clientfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0)
-	return -1;
+        return -1;
     return clientfd;
 }
 /* $end open_clientfd */
@@ -763,12 +763,12 @@ int open_listenfd(int port)
 
     /* Create a socket descriptor */
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	return -1;
+        return -1;
 
     /* Eliminates "Address already in use" error from bind. */
     if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
-		   (const void *)&optval , sizeof(int)) < 0)
-	return -1;
+                   (const void *)&optval , sizeof(int)) < 0)
+        return -1;
 
     /* Listenfd will be an endpoint for all requests to port
        on any IP address for this host */
@@ -777,11 +777,11 @@ int open_listenfd(int port)
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons((unsigned short)port);
     if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
-	return -1;
+        return -1;
 
     /* Make it a listening socket ready to accept connection requests */
     if (listen(listenfd, LISTENQ) < 0)
-	return -1;
+        return -1;
     return listenfd;
 }
 /* $end open_listenfd */
@@ -794,10 +794,10 @@ int Open_clientfd(char *hostname, int port)
     int rc;
 
     if ((rc = open_clientfd(hostname, port)) < 0) {
-	if (rc == -1)
-	    unix_error("Open_clientfd Unix error");
-	else
-	    dns_error("Open_clientfd DNS error");
+        if (rc == -1)
+            unix_error("Open_clientfd Unix error");
+        else
+            dns_error("Open_clientfd DNS error");
     }
     return rc;
 }
@@ -807,7 +807,7 @@ int Open_listenfd(int port)
     int rc;
 
     if ((rc = open_listenfd(port)) < 0)
-	unix_error("Open_listenfd error");
+        unix_error("Open_listenfd error");
     return rc;
 }
 /* $end csapp.c */
