eb4c38436f004b96b86370f3426dc521ffc95bb3
mofaph <mofaph@gmail.com>
2012-09-06 10:06:25 +0800

    show-bytes.c: 删除测试函数
    测试函数应该位于其他地方，show-bytes.c 应该只提供显示字节内容的函数。

--- show-bytes.c
+++ show-bytes.c
@@ -22,25 +22,3 @@ void show_float(float x) {
 void show_pointer(void *x) {
   show_bytes((byte_pointer) &x, sizeof(void *));
 }
-
-int main(void)
-{
-  int a = 12345;
-  int *b;
-  char c[4];
-  char *cp = "90\0\0";
-
-  c[0] = 57;
-  c[1] = 48;
-  c[2] = 0;
-  c[3] = 0;
-
-  show_bytes((unsigned char *)&c[0], 4);
-  b = (int *)&c[0];
-  printf("%d\n", *b);
-  b = (int *)cp;
-  printf("%d\n", *b);
-  show_bytes((unsigned char *)cp, 4);
-  show_int(a);
-  return 0;
-}
