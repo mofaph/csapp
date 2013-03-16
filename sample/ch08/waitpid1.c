c368086b455b84a3debcf16033fb61d243764b44
mofaph <mofaph@gmail.com>
2013-03-16 12:39:16 +0800

    sample: ch08: waitpid1.c: 改为 Linux 编码风格

--- sample/ch08/waitpid1.c
+++ sample/ch08/waitpid1.c
@@ -9,26 +9,26 @@
 
 int main(void)
 {
-    int status, i;
-    pid_t pid;
+        int status, i;
+        pid_t pid;
 
-    /* Parent creates N children */
-    for (i = 0; i < N; i++)
-        if ((pid = Fork()) == 0) /* Child */
-            exit(100+i);
+        /* Parent creates N children */
+        for (i = 0; i < N; i++)
+                if ((pid = Fork()) == 0) /* Child */
+                        exit(100+i);
 
-    /* Parent reaps N children in no particular order */
-    while ((pid = waitpid(-1, &status, 0)) > 0) {
-        if (WIFEXITED(status))
-            printf("child %d terminated normally with exit status=%d\n",
-                   pid, WEXITSTATUS(status));
-        else
-            printf("child %d terminated abnormally\n", pid);
-    }
+        /* Parent reaps N children in no particular order */
+        while ((pid = waitpid(-1, &status, 0)) > 0) {
+                if (WIFEXITED(status))
+                        printf("child %d terminated normally with exit status=%d\n",
+                               pid, WEXITSTATUS(status));
+                else
+                        printf("child %d terminated abnormally\n", pid);
+        }
 
-    /* The normal termination is if there are no more children */
-    if (errno != ECHILD)
-        unix_error("waitpid error");
+        /* The normal termination is if there are no more children */
+        if (errno != ECHILD)
+                unix_error("waitpid error");
 
-    exit(0);
+        exit(0);
 }
