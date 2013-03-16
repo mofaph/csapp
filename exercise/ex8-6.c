5fab82d2bd18339068e919e361156cd2665a7ec5
mofaph <mofaph@gmail.com>
2013-03-16 15:15:09 +0800

    exercise: 8.6: 加入详细的注释，同时更改编码风格为 Linux 风格

--- exercise/ex8-6.c
+++ exercise/ex8-6.c
@@ -1,25 +1,26 @@
 /*
- * myecho.c
+ * 8.6 -- p502
  *
  * 编写一个叫做 myecho 的程序，它打印出它的命令行参数和环境变量
+ *
+ * unix> cc ex8-6.c -o myecho
  */
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-  int i;
-  extern char **environ;
+        int i;
 
-  printf("Command line arguments:\n");
-  for (i = 0; i < argc; i++)
-    printf("argv[%d]: %s\n", i, argv[i]);
+        printf("Command line arguments:\n");
+        for (i = 0; i < argc; i++)
+                printf("\targv[%d]: %s\n", i, argv[i]);
+        printf("\n");
 
-  printf("Environment variables:\n");
-  for (i = 0; environ[i] != NULL; i++)
-    printf("envp[%d]: %s\n", i, environ[i]);
+        printf("Environment variables:\n");
+        extern char **environ;
+        for (i = 0; environ[i] != NULL; i++)
+                printf("\tenvp[%2d]: %s\n", i, environ[i]);
 
-  return 0;
+        return 0;
 }
-
-/* myecho.c ends here */
