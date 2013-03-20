5e1ea1b3906bbf753074ef00ab431d48eae79eb9
mofaph <mofaph@gmail.com>
2013-03-20 13:53:15 +0800

    sample: ch08: signal3.c: 调整编码风格

--- sample/ch08/signal3.c
+++ sample/ch08/signal3.c
@@ -2,43 +2,43 @@
 
 void handler2(int sig)
 {
-  pid_t pid;
+        pid_t pid;
 
-  while ((pid = waitpid(-1, NULL, 0)) > 0)
-    printf("Handler reaped child %d\n", (int)pid);
-  if (errno != ECHILD)
-    unix_error("waitpid error");
-  Sleep(2);
-  return;
+        while ((pid = waitpid(-1, NULL, 0)) > 0)
+                printf("Handler reaped child %d\n", (int)pid);
+        if (errno != ECHILD)
+                unix_error("waitpid error");
+        Sleep(2);
+        return;
 }
 
 int main()
 {
-  int i, n;
-  char buf[MAXBUF];
-  pid_t pid;
+        int i, n;
+        char buf[MAXBUF];
+        pid_t pid;
 
-  if (signal(SIGCHLD, handler2) == SIG_ERR)
-    unix_error("signal error");
+        if (signal(SIGCHLD, handler2) == SIG_ERR)
+                unix_error("signal error");
 
-  /* Parent creates children */
-  for (i = 0; i < 3; i++) {
-    pid = Fork();
-    if (pid == 0) {
-      printf("Hello from child %d\n", (int)getpid());
-      Sleep(1);
-      exit(0);
-    }
-  }
+        /* Parent creates children */
+        for (i = 0; i < 3; i++) {
+                pid = Fork();
+                if (pid == 0) {
+                        printf("Hello from child %d\n", (int)getpid());
+                        Sleep(1);
+                        exit(0);
+                }
+        }
 
-  /* Manually restart the read call if it is interrupted */
-  while ((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0)
-    if (errno != EINTR)
-      unix_error("read");
+        /* Manually restart the read call if it is interrupted */
+        while ((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0)
+                if (errno != EINTR)
+                        unix_error("read");
 
-  printf("Parent processing input\n");
-  while (1)
-    ;
+        printf("Parent processing input\n");
+        while (1)
+                ;
 
-  exit(0);
+        exit(0);
 }
