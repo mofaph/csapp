63fa5dd1733829c6ab7ee7a82a4e3e8399feee85
mofaph <mofaph@gmail.com>
2013-03-21 16:34:02 +0800

    sample: ch08: setjmp.c: 调整编码风格

--- sample/ch08/setjmp.c
+++ sample/ch08/setjmp.c
@@ -9,30 +9,30 @@ void foo(void), bar(void);
 
 int main()
 {
-  int rc;
+        int rc;
 
-  rc = setjmp(buf);
-  if (rc == 0)
-    foo();
-  else if (rc == 1)
-    printf("Detected an error1 condition in foo\n");
-  else if (rc == 2)
-    printf("Detected an error2 condition in foo\n");
-  else
-    printf("Unknown error condition in foo\n");
-  exit(0);
+        rc = setjmp(buf);
+        if (rc == 0)
+                foo();
+        else if (rc == 1)
+                printf("Detected an error1 condition in foo\n");
+        else if (rc == 2)
+                printf("Detected an error2 condition in foo\n");
+        else
+                printf("Unknown error condition in foo\n");
+        exit(0);
 }
 
 /* Deeply nested function foo */
 void foo(void)
 {
-  if (error1)
-    longjmp(buf, 1);
-  bar();
+        if (error1)
+                longjmp(buf, 1);
+        bar();
 }
 
 void bar(void)
 {
-  if (error2)
-    longjmp(buf, 2);
+        if (error2)
+                longjmp(buf, 2);
 }
