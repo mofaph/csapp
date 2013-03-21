fca06e8e52bc7358f4899051a19b9e29a73750d3
mofaph <mofaph@gmail.com>
2013-03-21 13:11:10 +0800

    sample: ch08: procmask2.c: 调整编码风格

--- sample/ch08/procmask2.c
+++ sample/ch08/procmask2.c
@@ -2,36 +2,36 @@
 
 void handler(int sig)
 {
-  pid_t pid;
-  while ((pid = waitpid(-1, NULL, 0)) > 0) /* Reap a zombie child */
-    deletejob(pid);                        /* Delete the child from the job list */
-  if (errno != ECHILD)
-    unix_error("waitpid error");
+        pid_t pid;
+        while ((pid = waitpid(-1, NULL, 0)) > 0) /* Reap a zombie child */
+                deletejob(pid); /* Delete the child from the job list */
+        if (errno != ECHILD)
+                unix_error("waitpid error");
 }
 
 int main(int argc, char **argv)
 {
-  int pid;
-  sigset_t mask;
+        int      pid;
+        sigset_t mask;
 
-  Signal(SIGCHLD, handler);
-  initjobs();                   /* Initialize the job list */
+        Signal(SIGCHLD, handler);
+        initjobs();                   /* Initialize the job list */
 
-  while (1) {
-    Sigemptyset(&mask);
-    Sigaddset(&mask, SIGCHLD);
-    Sigprocmask(SIG_BLOCK, &mask, NULL); /* Block SIGCHLD */
+        while (1) {
+                Sigemptyset(&mask);
+                Sigaddset(&mask, SIGCHLD);
+                Sigprocmask(SIG_BLOCK, &mask, NULL); /* Block SIGCHLD */
 
-    /* Child process */
-    if ((pid = Fork()) == 0) {
-      Sigprocmask(SIG_UNBLOCK, &mask, NULL); /* Unblock SIGCHLD */
-      Execve("/bin/date", argv, NULL);
-    }
+                /* Child process */
+                if ((pid = Fork()) == 0) {
+                        Sigprocmask(SIG_UNBLOCK, &mask, NULL); /* Unblock SIGCHLD */
+                        Execve("/bin/date", argv, NULL);
+                }
 
-    /* Parent process */
-    addjob(pid);                /* Add the child to the job list */
-    Sigprocmask(SIG_UNBLOCK, &mask, NULL); /* Unblock SIGCHLD */
-  }
+                /* Parent process */
+                addjob(pid);    /* Add the child to the job list */
+                Sigprocmask(SIG_UNBLOCK, &mask, NULL); /* Unblock SIGCHLD */
+        }
 
-  exit(0);
+        exit(0);
 }
