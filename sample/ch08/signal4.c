d4df04339e0d17abe6120e44d268d2d22cced198
mofaph <mofaph@gmail.com>
2013-03-21 12:34:04 +0800

    sample: ch08: signal4.c: 调整编码风格

--- sample/ch08/signal4.c
+++ sample/ch08/signal4.c
@@ -2,41 +2,41 @@
 
 void handler2(int sig)
 {
-  pid_t pid;
+        pid_t pid;
 
-  while ((pid = waitpid(-1, NULL, 0)) > 0)
-    printf("Handler reaped child %d\n", (int)pid);
-  if (errno != ECHILD)
-    unix_error("waitpid error");
-  Sleep(2);
-  return;
+        while ((pid = waitpid(-1, NULL, 0)) > 0)
+                printf("Handler reaped child %d\n", (int)pid);
+        if (errno != ECHILD)
+                unix_error("waitpid error");
+        Sleep(2);
+        return;
 }
 
 int main()
 {
-  int i, n;
-  char buf[MAXBUF];
-  pid_t pid;
+        int i, n;
+        char buf[MAXBUF];
+        pid_t pid;
 
-  Signal(SIGCHLD, handler2);    /* sigaction error-handling wrapper */
+        Signal(SIGCHLD, handler2); /* sigaction error-handling wrapper */
 
-  /* Parent creates children */
-  for (i = 0; i < 3; i++) {
-    pid = Fork();
-    if (pid == 0) {
-      printf("Hello from child %d\n", (int)getpid());
-      Sleep(1);
-      exit(0);
-    }
-  }
+        /* Parent creates children */
+        for (i = 0; i < 3; i++) {
+                pid = Fork();
+                if (pid == 0) {
+                        printf("Hello from child %d\n", (int)getpid());
+                        Sleep(1);
+                        exit(0);
+                }
+        }
 
-  /* Parent waits for terminal input and then processes it */
-  if ((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0)
-    unix_error("read");
+        /* Parent waits for terminal input and then processes it */
+        if ((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0)
+                unix_error("read");
 
-  printf("Parent processing input\n");
-  while (1)
-    ;
+        printf("Parent processing input\n");
+        while (1)
+                ;
 
-  exit(0);
+        exit(0);
 }
