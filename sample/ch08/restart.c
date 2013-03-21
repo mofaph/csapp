5bb5c6e172b22c8363a364e413ec386752610384
mofaph <mofaph@gmail.com>
2013-03-21 16:46:22 +0800

    sample: ch08: restart.c: 增加说明性注释

--- sample/ch08/restart.c
+++ sample/ch08/restart.c
@@ -4,21 +4,21 @@ sigjmp_buf buf;
 
 void handler(int sig)
 {
-  siglongjmp(buf, 1);
+        siglongjmp(buf, 1);
 }
 
 int main()
 {
-  Signal(SIGINT, handler);
+        Signal(SIGINT, handler);
 
-  if (!sigsetjmp(buf, 1))
-    printf("starting\n");
-  else
-    printf("restarting\n");
+        if (!sigsetjmp(buf, 1))
+                printf("starting\n");
+        else
+                printf("restarting\n");
 
-  while (1) {
-    Sleep(1);
-    printf("processing...\n");
-  }
-  exit(0);
+        while (1) {
+                Sleep(1);
+                printf("processing...\n");
+        }
+        exit(0);
 }
