4b7a44f7c062d532815b8a7320bc4268c12e422d
mofaph <mofaph@gmail.com>
2013-03-22 11:28:23 +0800

    sample: ch02: show-bytes.c: 调整编码风格

--- sample/ch02/show-bytes.c
+++ sample/ch02/show-bytes.c
@@ -4,21 +4,25 @@
 
 typedef unsigned char *byte_pointer;
 
-void show_bytes(byte_pointer start, int len) {
-  int i;
-  for (i = 0; i < len; i++)
-    printf(" %.2x", start[i]);
-  printf("\n");
+void show_bytes(byte_pointer start, int len)
+{
+        int i;
+        for (i = 0; i < len; i++)
+                printf(" %.2x", start[i]);
+        printf("\n");
 }
 
-void show_int(int x) {
-  show_bytes((byte_pointer) &x, sizeof(int));
+void show_int(int x)
+{
+        show_bytes((byte_pointer) &x, sizeof(int));
 }
 
-void show_float(float x) {
-  show_bytes((byte_pointer) &x, sizeof(float));
+void show_float(float x)
+{
+        show_bytes((byte_pointer) &x, sizeof(float));
 }
 
-void show_pointer(void *x) {
-  show_bytes((byte_pointer) &x, sizeof(void *));
+void show_pointer(void *x)
+{
+        show_bytes((byte_pointer) &x, sizeof(void *));
 }
