919205e1c59611a9a7fd4c0877864cb6fdb6149e
mofaph <mofaph@gmail.com>
2013-08-10 23:41:02 +0800

    sample/ch12/sharing.c: 消除一个编译器警告，增加注释

--- sample/ch12/sharing.c
+++ sample/ch12/sharing.c
@@ -1,9 +1,13 @@
 /*
  * p662 -- code/conc/sharing.c
  *
+ * 这段代码用来说明关于多线程程序中的共享变量。下面的代码由一个创建了两个对等线程
+ * 的主线程组成。主线程传递一个唯一的 ID 给每个对等线程，每个对等线程利用这个 ID
+ * 输出一条个性化的信息，以及调用该线程例程的总次数。
+ *
  * 在 UNIX 系统下编译和运行：
  *
- * unix> make sharing
+ * unix> make sharing # 编译时可能会有警告，可以忽略
  * unix> ./sharing
  */
 
@@ -28,6 +32,7 @@ int main()
         for (i = 0; i < N; i++)
                 Pthread_create(&tid, NULL, thread, (void *)i);
         Pthread_exit(NULL);
+        return 0;
 }
 
 void *thread(void *vargp)
